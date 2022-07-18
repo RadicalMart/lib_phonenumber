1bf876baedf5e6e9c499276f01a999bca13fcea1
Artem Vasilev <kernusr@users.noreply.github.com>
2022-07-18 16:25:04 +0300

    Joomla implementation (#1)
    * Добавил файлы, необходимые для установки библиотеки в Joomla
    
    * Перенёс файлы, необходимые для установки библиотеки в Joomla
    
    * Добавил плагин-загрузчик, для загрузки библиотеки в Joomla
    
    * Добавил метапакет, для установки библиотеки и плагина-загрузчика в Joomla
    
    * Поправил языки для плагина-загрузчика
    
    * Поправил языки для плагина-загрузчика
    
    * Поправил название библиотеки для установки в joomla
    
    * Поправил путь к библиотеке в joomla
    
    * bash скрипт для сборки установочного архива пакета с бибилиотекой
    
    * Упаковка архива с релизом в github actions
    
    * test
    
    * Правка путей в скриптах сборщика
    
    * Версия и копирайт

---
+++ joomla/plugin/phonenumber.php
@@ -0,0 +1,24 @@
+<?php
+
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Plugin\CMSPlugin;
+
+defined('_JEXEC') or die;
+
+class PlgSystemPhonenumber extends CMSPlugin
+{
+	/**
+	 * Load the language file on instantiation.
+	 *
+	 * @var    boolean
+	 */
+	protected $autoloadLanguage = true;
+
+	public function onAfterInitialise()
+	{
+		if (!@include_once(JPATH_LIBRARIES . '/libphonenumber/vendor/autoload.php'))
+		{
+			throw new \RuntimeException(Text::_('PLG_SYSTEM_PHONENUMBER_ERROR_AUTOLOADER_NOT_LOAD'), 1);
+		}
+	}
+}
