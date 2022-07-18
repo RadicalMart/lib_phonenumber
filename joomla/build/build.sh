96b7d22a1ae536bcc00ebad003647bc72449098b
Artem Vasilev <kern.usr@gmail.com>
2022-07-18 15:58:11 +0300

    Правка путей в скриптах сборщика

--- joomla/build/build.sh
+++ joomla/build/build.sh
@@ -1,10 +1,12 @@
-LIB_PATH_ROOT=$(realpath $PWD/../..)
-LIB_PATH_LIBRARY=$(realpath $LIB_PATH_ROOT/joomla/library)
-LIB_PATH_PLUGIN=$(realpath $LIB_PATH_ROOT/joomla/plugin)
+#!/usr/bin/env bash
+
+LIB_PWD=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+LIB_PATH_ROOT=$(realpath $LIB_PWD/../..)
 LIB_PATH_PKG=$(realpath $LIB_PATH_ROOT/joomla)
+LIB_PATH_LIBRARY=$(realpath $LIB_PATH_PKG/library)
+LIB_PATH_PLUGIN=$(realpath $LIB_PATH_PKG/plugin)
 
 cp -r $LIB_PATH_ROOT/LICENSE $LIB_PATH_ROOT/src $LIB_PATH_ROOT/vendor $LIB_PATH_LIBRARY
 
-ls -l $LIB_PATH_LIBRARY
-
-(cd $LIB_PATH_PKG && zip -r - library plugin language pkg_phonenumber.xml) > $PWD/pkg_phonenumber.zip
+(cd $LIB_PATH_PKG && zip -r - library plugin language pkg_phonenumber.xml) > $LIB_PWD/pkg_phonenumber.zip
