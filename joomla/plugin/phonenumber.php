bf134f56c2eeb63628c2b075471e40b0f5fbc20a
Artem Vasilev <kern.usr@gmail.com>
2022-07-18 12:42:18 +0300

    Добавил плагин-загрузчик, для загрузки библиотеки в Joomla

---
+++ joomla/plugin/phonenumber.php
@@ -0,0 +1,25 @@
+<?php
+
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Plugin\CMSPlugin;
+
+defined('_JEXEC') or die;
+
+class PlgSystemPhonenumber extends CMSPlugin
+{
+	/**
+	 * Load the language file on instantiation.
+	 *
+	 * @var    boolean
+	 * @since  __DEPLOY_VERSION__
+	 */
+	protected $autoloadLanguage = true;
+
+	public function onAfterInitialise()
+	{
+		if (!@include_once(JPATH_LIBRARIES . '/lib_phonenumber/vendor/autoload.php'))
+		{
+			throw new \RuntimeException(Text::_('PLG_SYSTEM_PHONENUMBER_ERROR_AUTOLOADER_NOT_LOAD'), 1);
+		}
+	}
+}
