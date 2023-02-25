10917ef99a9ef7cf1582620d28f0defe7951b216
Artem Vasilev <kern.usr@gmail.com>
2023-02-26 00:50:52 +0300

    replace indent to tab

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,34 +1,35 @@
 <?php
 
 return (new PhpCsFixer\Config())
-    ->setFinder(
-        \Symfony\Component\Finder\Finder::create()
-            ->in('src')
-            ->in('tests')
-            ->name('*.php')
-    )
-    ->setRiskyAllowed(true)
-    ->setRules([
-        '@PSR12' => true,
-        'array_indentation' => true,
-        'declare_strict_types' => false,
-        'final_class' => false,
-        'global_namespace_import' => [
-            'import_classes' => true,
-            'import_constants' => true,
-            'import_functions' => true,
-        ],
-        'list_syntax' => [
-            'syntax' => 'short',
-        ],
-        'constant_case' => [
-            'case' => 'lower',
-        ],
-        'no_unused_imports' => true,
-        'no_useless_else' => true,
-        'no_useless_return' => true,
-        'ordered_imports' => [
-            'imports_order' => ['class', 'function', 'const'],
-        ],
-        'single_import_per_statement' => true,
-    ]);
+	->setIndent("\t")
+	->setFinder(
+		\Symfony\Component\Finder\Finder::create()
+			->in('src')
+			->in('tests')
+			->name('*.php')
+	)
+	->setRiskyAllowed(true)
+	->setRules([
+		'@PSR12' => true,
+		'array_indentation' => true,
+		'declare_strict_types' => false,
+		'final_class' => false,
+		'global_namespace_import' => [
+			'import_classes' => true,
+			'import_constants' => true,
+			'import_functions' => true,
+		],
+		'list_syntax' => [
+			'syntax' => 'short',
+		],
+		'constant_case' => [
+			'case' => 'lower',
+		],
+		'no_unused_imports' => true,
+		'no_useless_else' => true,
+		'no_useless_return' => true,
+		'ordered_imports' => [
+			'imports_order' => ['class', 'function', 'const'],
+		],
+		'single_import_per_statement' => true,
+	]);
--- src/PhoneNumber.php
+++ src/PhoneNumber.php
@@ -10,88 +10,88 @@ use RadicalMart\PhoneNumber\Exception\InvalidRegionCodeException;
 
 class PhoneNumber extends \libphonenumber\PhoneNumber
 {
-    /**
-     * Код страны. Устанавливает правила разбора и форматирования номера телефона
-     *
-     * @var string
-     */
-    protected $region;
+	/**
+	 * Код страны. Устанавливает правила разбора и форматирования номера телефона
+	 *
+	 * @var string
+	 */
+	protected $region;
 
-    /**
-     * Код языка. Устанавливает код языка, на котором будет выводиться информация о номере телефона
-     *
-     * @var string
-     */
-    protected $language_code;
+	/**
+	 * Код языка. Устанавливает код языка, на котором будет выводиться информация о номере телефона
+	 *
+	 * @var string
+	 */
+	protected $language_code;
 
-    /**
-     * Экземпляр класса утилиты разбора номеров телефонов
-     *
-     * @var PhoneNumberUtil
-     */
-    protected $phone_util;
+	/**
+	 * Экземпляр класса утилиты разбора номеров телефонов
+	 *
+	 * @var PhoneNumberUtil
+	 */
+	protected $phone_util;
 
-    /**
-     * @param string $input Входящая строка, содержит номер телефона, который необходимо отформатированть
-     * @param string $region Код страны. Устанавливает правила разбора и форматирования номера телефона
-     * @param string $language_code Код языка. Устанавливает код языка, на котором будет выводиться информация о номере телефона
-     *
-     * @throws \libphonenumber\NumberParseException
-     */
-    public function __construct(string $input, string $region = 'RU', string $language_code = 'ru-RU')
-    {
-        $this->region = $region;
-        $this->language_code = $language_code;
-        $this->phone_util = PhoneNumberUtil::getInstance();
+	/**
+	 * @param string $input Входящая строка, содержит номер телефона, который необходимо отформатированть
+	 * @param string $region Код страны. Устанавливает правила разбора и форматирования номера телефона
+	 * @param string $language_code Код языка. Устанавливает код языка, на котором будет выводиться информация о номере телефона
+	 *
+	 * @throws \libphonenumber\NumberParseException
+	 */
+	public function __construct(string $input, string $region = 'RU', string $language_code = 'ru-RU')
+	{
+		$this->region = $region;
+		$this->language_code = $language_code;
+		$this->phone_util = PhoneNumberUtil::getInstance();
 
-        $this->phone_util->parse($input, $this->region, $this, true);
+		$this->phone_util->parse($input, $this->region, $this, true);
 
-        if (!$this->phone_util->isValidNumber($this)) {
-            throw new InvalidRegionCodeException();
-        }
-    }
+		if (!$this->phone_util->isValidNumber($this)) {
+			throw new InvalidRegionCodeException();
+		}
+	}
 
-    /**
-     * Вернёт отформатированную строку с номером телефона
-     *
-     * @param int $numberFormat Индекс формата
-     *
-     * @return string
-     */
-    public function format(int $numberFormat = PhoneNumberFormat::E164): string
-    {
-        return $this->phone_util->format($this, $numberFormat);
-    }
+	/**
+	 * Вернёт отформатированную строку с номером телефона
+	 *
+	 * @param int $numberFormat Индекс формата
+	 *
+	 * @return string
+	 */
+	public function format(int $numberFormat = PhoneNumberFormat::E164): string
+	{
+		return $this->phone_util->format($this, $numberFormat);
+	}
 
-    /**
-     * Вернёт данные о привязке номера к региону
-     *
-     * @param string|null $language_code Код языка, на котором будет выводиться информация о привязке номера к региону
-     *
-     * @return string
-     */
-    public function geocode(string $language_code = null): string
-    {
-        if (empty($language_code)) {
-            $language_code = $this->language_code;
-        }
+	/**
+	 * Вернёт данные о привязке номера к региону
+	 *
+	 * @param string|null $language_code Код языка, на котором будет выводиться информация о привязке номера к региону
+	 *
+	 * @return string
+	 */
+	public function geocode(string $language_code = null): string
+	{
+		if (empty($language_code)) {
+			$language_code = $this->language_code;
+		}
 
-        return PhoneNumberOfflineGeocoder::getInstance()->getDescriptionForNumber($this, $language_code);
-    }
+		return PhoneNumberOfflineGeocoder::getInstance()->getDescriptionForNumber($this, $language_code);
+	}
 
-    /**
-     * Вернёт данные о привязке номера к оператору связи
-     *
-     * @param string|null $language_code Код языка, на котором будет выводиться информация о привязке номера к оператору связи
-     *
-     * @return string
-     */
-    public function carrier(string $language_code = null): string
-    {
-        if (empty($language_code)) {
-            $language_code = $this->language_code;
-        }
+	/**
+	 * Вернёт данные о привязке номера к оператору связи
+	 *
+	 * @param string|null $language_code Код языка, на котором будет выводиться информация о привязке номера к оператору связи
+	 *
+	 * @return string
+	 */
+	public function carrier(string $language_code = null): string
+	{
+		if (empty($language_code)) {
+			$language_code = $this->language_code;
+		}
 
-        return PhoneNumberToCarrierMapper::getInstance()->getNameForNumber($this, $language_code);
-    }
+		return PhoneNumberToCarrierMapper::getInstance()->getNameForNumber($this, $language_code);
+	}
 }
